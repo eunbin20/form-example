ec0a290d9457f407011e983529680468a6b95703
kim-eunbin <eunbin.kim@thingsflow.com>
2022-12-04 21:45:03 +0900

    react-hook-form 방식

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,90 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function Form() {
-  // react state를 이용한 input value 관리
-  const [passwordInputValue, setPasswordInputValue] = useState<string>("");
-  const [emailInputValue, setEmailInputValue] = useState<string>("");
-  const [checkedTerms1, setCheckedTerms1] = useState<boolean>(false);
+type FormValues = {
+  email: string;
+  password: string;
+  terms: string;
+};
 
-  // 유효성 검사 결과도 state로 관리
-  const [validationMessage, setValidationMessage] = useState<string>("");
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
     const result = {
-      email: emailInputValue,
-      password: passwordInputValue,
+      email: data.email,
+      password: data.password,
+      terms: data.terms,
     };
 
     alert("submit completed!" + JSON.stringify(result));
   };
 
-  const emailValidator = (value: string) => {
-    const regex = /^[\w\W-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-    if (value.length === 0) {
-      setValidationMessage("이메일은 필수입니다");
-    } else {
-      if (regex.test(value)) {
-        setValidationMessage("");
-      } else {
-        setValidationMessage("유효한 이메일 양식이 아닙니다");
-      }
-    }
-  };
-
-  const passwordValidator = (value: string) => {
-    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&_])[\w\W]{8,}$/;
-    return regex.test(value) || "8자리 이상의 영문, 숫자, 특수문자";
-  };
-
-  const handleEmailInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setEmailInputValue(e.currentTarget.value);
-    emailValidator(e.currentTarget.value);
-  };
-
   return (
     <div className="form-container">
       <h1>A Register Form using only React</h1>
-      <h2>이것은 리액트만 이용해서 만든 회원가입 폼~</h2>
-      <form onSubmit={handleSubmit} className="register-form">
+      <h2>react-hook-form을 활용해보았다!</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="register-form">
         <div className="form-item">
           <label htmlFor="email">Email: </label>
           <input
-            id="email"
             type="text"
-            value={emailInputValue}
-            onChange={handleEmailInputChange}
-            autoComplete="off"
-            pattern="[A-Za-z-\.]+@([a-z-]+\.)+[\w-]{2,4}"
-            required
+            {...register("email", {
+              required: {
+                value: true,
+                message: "이메일은 필수입니다",
+              },
+              pattern: {
+                value: /^[A-Za-z-.]+@([a-z-]+.)+[w-]{2,4}/i,
+                message: "유효한 이메일 양식이 아닙니다",
+              },
+            })}
           />
-          <p className="error-message">이메일을 확인해주세요</p>
+          <p className="error-message">{errors?.email?.message}</p>
         </div>
         <div className="form-item">
           <label htmlFor="password">Password: </label>
           <input
-            id="password"
             type="password"
-            value={passwordInputValue}
-            onChange={(e) => setPasswordInputValue(e.currentTarget.value)}
+            {...register("password", {
+              pattern: {
+                value: /^[A-Za-z-.]+@([a-z-]+.)+[w-]{2,4}/i,
+                message: "8자리 이상의 영문, 숫자, 특수문자",
+              },
+            })}
           />
-          <p className="error-message">비밀번호를 확인해주세요</p>
+          <p className="error-message">{errors?.password?.message}</p>
         </div>
         <div className="form-item">
-          <input
-            id="terms"
-            type="checkbox"
-            checked={checkedTerms1}
-            onChange={() => setCheckedTerms1((prev) => !prev)}
-          />
+          <input type="checkbox" {...(register("terms"), { required: true })} />
           <label htmlFor="terms" className="checkbox-label">
             terms content...
           </label>
         </div>
-
         <div className="form-item">
           <input type="submit" value="제출" />
         </div>
@@ -92,5 +73,3 @@ function Form() {
     </div>
   );
 }
-
-export default Form;
